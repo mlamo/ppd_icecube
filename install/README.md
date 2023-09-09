2635ce8d92b89f912cd228c0b33ce8d5a7ecd3f2
Christoph Raab <chraab@mailbox.org>
2023-09-09 19:20:54 +0200

    install: replace curl by wget and move checklist to separate file

---
+++ install/README.md
@@ -0,0 +1,21 @@
+Assume we are starting from a fresh install of Ubuntu and Python 3.
+
+# Checklist for before (not yet automated)
+
+* set password (beware of AZERTY)
+* add QWERTY layout to settings
+* connect to WiFi (need the sudoer password)
+
+# run install.sh
+
+# Checklist for afterwards (not yet automated)
+
+* add jupyter.desktop launcher to favorites
+* test that Jupyter launcher works
+* set Jupyter language to French
+* make a copy of ppd/
+* open the notebooks therein from the file manager with jupyter.desktop (setting it as default)
+* run them all
+* close and delete the copy of ppd
+* turn down the volume
+* set keyboard layout back to AZERTY
--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # paths and definitions
 desktop_path=${HOME}/Bureau/
 usb_path=/media/${USER}/C7D4-163E
@@ -9,9 +10,11 @@ source ~/.bashrc
 
 # Python packages
 #python3 ${usb_path}/install/get-pip.py
-curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# curl is not installed by default, wth
+wget https://bootstrap.pypa.io/get-pip.py 
 python3 get-pip.py
-pip install --user jupyter numpy healpy matplotlib pandas
+pip install --user jupyter numpy healpy matplotlib pandas astropy
 pip install --user --upgrade scipy
 pip install --user jupyterlab-language-pack-fr-FR
 
@@ -21,18 +24,10 @@ cp ${usb_path}/install/ppd.sh ~/.local/bin/
 chmod +x ~/.local/bin/ppd.sh
 # can execute it from command line or context menu
 # but warn them not to run it twice (TODO check does Jupyter 7 prevent double launches?)
-cp ${usb_path}/install/PPD.desktop ~/.local/share/applications
-chmod +x ~/.local/share/applications/PPD.desktop
 cp ${usb_path}/install/logo.png ~/.local/share/applications/
 desktop-file-install --dir="$HOME/.local/share/applications"  ${usb_path}/install/jupyter.desktop
 
 # tests
-python3 ${desktop_path}/ppd/fonctions.py
-python3 ${usb_path}/install/versions.py
+python3 ${desktop_path}/ppd/fonctions.py # imports work?
+python3 ${usb_path}/install/versions.py # show version numbers
 
-# afterwards (not yet automated)
-# add QWERTY layout to settings
-# change setting in file manager to open ipynb with jupyter.desktop
-# add jupyter.desktop to favorites
-# launch Jupyter, set language to French
-# set keyboard layout back to AZERTY
